ec4373aba96d1ee7ad7be9c99cec78db61f38efc
escharf320 <127362654+escharf320@users.noreply.github.com>
2023-10-23 15:41:18 -0400

    fixing path to kspdg-gym

--- arclab_mit/agents/pure_text_agent.py
+++ arclab_mit/agents/pure_text_agent.py
@@ -1,18 +1,23 @@
+import sys
 import openai
 import os
+
+kspdg_path = "/Users/eli/SPACEGYM-KSPDG/src/kspdg/base_agent.py"
+sys.path.append(kspdg_path)
+
 from kspdg.agent_api.base_agent import KSPDGBaseAgent
 from kspdg.pe1.e1_envs import PE1_E1_I3_Env
 from kspdg.agent_api.runner import AgentEnvRunner
 
 
-class TextBasedAgent(KSPDGBaseAgent):
+class TextBasedAgent:  # (KSPDGBaseAgent)
     def __init__(self):
         self.api_key = os.environ.get("OPENAI_API_KEY")
         if self.api_key == None:
             self.api_key = input("Please enter your OPENAI_API_KEY: ")
             if not self.api_key:
                 raise AssertionError("Please set the OPENAI_API_KEY variable")
-        self.system_prompt = ""  # TODO add prompt
+        self.system_prompt = "Hi, can you please generate a list of moves for me to take. For example maybe take two steps to the right, amd then four steps to the left. I want you return these moves in a vector form. Maybe use a json to best frame the data but I am leaving that decision up to you. Also please do not return any extra words."  # TODO add prompt
         self.observation_history = ""  # TODO add observations
 
     def generate_response(self):

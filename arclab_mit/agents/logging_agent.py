fe946aa3892a5b862adb845e01b23b24f84aeede
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-20 20:09:59 -0500

    move logging agent

--- arclab_mit/agents/logging_agent.py
+++ arclab_mit/agents/logging_agent.py
@@ -1,7 +1,10 @@
 from kspdg.agent_api.base_agent import KSPDGBaseAgent
 from kspdg.pe1.e1_envs import PE1_E1_I3_Env
-from kspdg.pe1.e3_envs import PE1_E3_I3_Env
 from kspdg.agent_api.runner import AgentEnvRunner
+import json
+import os
+
+output_json_path = os.path.join(os.path.dirname(__file__), 'outputs.json')
 
 class LoggingAgent(KSPDGBaseAgent):
     """
@@ -15,11 +18,12 @@ class LoggingAgent(KSPDGBaseAgent):
         return [0,0,0,5]
 
 if __name__ == "__main__":
-    runner = AgentEnvRunner(
-        agent=LoggingAgent(), 
-        env_cls=PE1_E1_I3_Env, 
-        env_kwargs=None,
-        runner_timeout=500,
-        debug=False
-        )
-    runner.run()
+    for _ in range(10):
+        runner = AgentEnvRunner(
+            agent=LoggingAgent(), 
+            env_cls=PE1_E1_I3_Env, 
+            env_kwargs=None,
+            runner_timeout=500,
+            debug=False
+            )
+        runner.run()

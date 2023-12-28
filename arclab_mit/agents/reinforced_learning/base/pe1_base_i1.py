6193a49a3c067ca526a5e48b907988dea849ba49
escharf320 <127362654+escharf320@users.noreply.github.com>
2023-12-28 18:02:05 -0500

    iterated base agent 2

--- arclab_mit/agents/reinforced_learning/base/pe1_base_i1.py
+++ arclab_mit/agents/reinforced_learning/base/pe1_base_i1.py
@@ -10,9 +10,14 @@ class NaivePursuitAgent(KSPDGBaseAgent):
     def __init__(self):
         super().__init__()
 
-    def get_action(self, state, gain=3.0):
-        position = np.array(state["position"])
-        velocity = np.array(state["velocity"])
+    def get_action(self, observation, gain=3.0):
+        print(observation)
+        state_dict = {
+            "position": observation[3:6],
+            "velocity": observation[6:9],
+        }
+        position = np.array(state_dict["position"])
+        velocity = np.array(state_dict["velocity"])
 
         relative_velocity = -velocity / np.linalg.norm(velocity)
 
@@ -24,11 +29,19 @@ class NaivePursuitAgent(KSPDGBaseAgent):
                 gain * np.linalg.norm(velocity) * line_of_sight_rate
             )
         else:
-            commanded_acceleration = np.array([0, 0, 0, 10])
+            commanded_acceleration = np.array([0, 0, 0])
 
-        return commanded_acceleration / np.linalg.norm(
-            commanded_acceleration, 1
-        )  # normalize
+        action = np.zeros(4)
+        if commanded_acceleration[0] > 0:
+            action[0] = 1
+        elif commanded_acceleration[0] < 0:
+            action[1] = 1
+        if commanded_acceleration[1] > 0:
+            action[2] = 1
+        elif commanded_acceleration[1] < 0:
+            action[3] = 1
+
+        return action
 
 
 if __name__ == "__main__":
